4e9be3d71592064fb9f1c2a949b0de5b56e3f885
谢明 <ming.xie@socialcredits.cn>
2018-12-04 15:59:53 +0800

    feat: append
    append会两倍容量扩容

---
+++ slice_append.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []int{}
+	fmt.Println(len(slice))
+	fmt.Println(cap(slice))
+
+	// 在原切片末尾添加元素，自动扩容
+	slice = append(slice, 6)
+	fmt.Println(len(slice))
+	fmt.Println(cap(slice))
+
+	// 如果超出原来的容量会已2倍容量扩容
+	slice2 := make([]int, 2)
+	slice2 = append(slice2, 14)
+	slice2 = append(slice2, 16)
+	fmt.Println("len =",len(slice2))
+	fmt.Println("cap",cap(slice2))
+	slice2 = append(slice2, 18)
+	fmt.Println("len =",len(slice2))
+	fmt.Println("cap",cap(slice2))
+
+}
