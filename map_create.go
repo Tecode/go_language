c2f96e617c335d236adbc17906c7ad26d45a77fb
Ming <ming@Mings-MacBook-Pro.local>
2019-01-06 13:36:41 +0800

    feat: map用法加入了说明

--- map_create.go
+++ map_create.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 )
 
+type Student struct {
+	id int
+	name string
+}
+
 func main() {
 	maps := map[string]string{"name":"mary","age":"12"}
 	fmt.Println(maps)
 	data, _ := json.Marshal(maps)
 	fmt.Println(string(data))
+	// 需要传每一个参数
+	var student = Student{5, "Ming"}
+	fmt.Println(student)
+	// 只需要传指定的参数
+	student2 := Student{name:"Aming"}
+	fmt.Println(student2)
 }
