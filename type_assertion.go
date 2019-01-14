188cdb9f187c1b547eec8946dde9b36644fc0a0e
谢明 <ming.xie@socialcredits.cn>
2019-01-14 16:44:17 +0800

    feat: 判断类型
    使用if&switch

---
+++ type_assertion.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Student struct {
+	name string
+}
+
+func main() {
+	var i []interface{} = make([]interface{}, 3)
+	i[0] = 5
+	i[1] = "string"
+	i[2] = Student{"Ming"}
+
+	for _, data := range i {
+		switch value := data.(type) {
+		case int:
+			fmt.Println("int", value)
+			break
+		case string:
+			fmt.Println("string", value)
+			break
+		case Student:
+			fmt.Println("Student", value)
+			break
+		}
+	}
+	// 第一个是值，第二个是bool值
+	if value, ok := i[0].(int); ok == true {
+		println(value, ok)
+	}
+
+}
