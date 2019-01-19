f56e1b3114a25f1272f1b87a6a6753f43fc300d0
Tecode <283731869@qq.com>
2019-01-19 20:46:56 +0800

    '补充json的一些操作，例如取别名，缩进'

--- json_struct_create.go
+++ json_struct_create.go
@@ -6,16 +6,18 @@ import (
 )
 
 type It struct {
-	Name    string
-	Subject []string
-	IsOk    bool
+	Name    string `json:"name"` // json:"name"设置别名
+	Subject []string `json:"subject"`
+	IsOk    bool `json:"isOk"`
 }
 
 func main() {
 	// 定义一个结构体变量
 	it := It{Name: "Go", Subject: []string{"C++", "Python", "Go", "Java", ""}, IsOk: true}
 	//	编码，更加内容生成json文本
-	buf, err := json.Marshal(it)
+	//buf, err := json.Marshal(it)
+	// MarshalIndent格式化，让代码能够有缩进
+	buf, err := json.MarshalIndent(it, "", " ")
 	if err != nil {
 		fmt.Println(err)
 		return
