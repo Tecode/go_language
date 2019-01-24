c5cfe1f0a7250fc7313e30c245e21986ea414236
Tecode <ming.xie@socialcredits.cn>
2019-01-24 15:26:58 +0800

    feat: go语言的...运算符

---
+++ function...operate.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func add(args ...int) int {
+	sum := 0
+	for _, arg := range args {
+		sum += arg
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(add(5))
+	fmt.Println(add([]int{5, 80, 9, 8, 5, 8, 9}...))
+}
