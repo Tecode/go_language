df30cc6992f00a211eaa5351ff21f7bbd8694b94
Tecode <283731869@qq.com>
2019-04-17 22:28:57 +0800

    '数组'

--- array.go
+++ array.go
@@ -5,9 +5,15 @@ import "fmt"
 func main() {
 	var list01[2] string
 	var array = []int{1, 5, 6, 9}
+	var user = []string{"45", "457"};
+	fmt.Println(user)
 
 	list01[0] = "hello"
 	list01[1] = "world"
+	for _,value := range list01[0] {
+		//fmt.Println(index, value)
+		fmt.Printf("%c \n", value)
+	}
 
 	list02 := []int{4, 2, 6, 8, 7}
 
